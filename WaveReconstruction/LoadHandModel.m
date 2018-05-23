0b8d247f42c7be010265c58cb582a55a0e0e4fca
maxwellre <yitianshao@gmail.com>
2018-05-22 22:12:14 -0700

    Reconstuct wave on a 3D scanned model.

---
+++ WaveReconstruction/LoadHandModel.m
@@ -0,0 +1,135 @@
+%% Loading vertices of a 3D model from a .obj file
+% Author: Yitian Shao (yitianshao@ucsb.edu)
+% Created on 09/05/2017
+% -------------------------------------------------------------------------
+close all
+clear all
+% clc
+% -------------------------------------------------------------------------
+% Configuration
+Skin_Color = [255,223,196]/255;
+Skin_Color2 = [255,205,148]/255;
+Hand_Truncate_Threshold = 40;
+Digit_Diameter = 16; 
+% -------------------------------------------------------------------------
+% Loading the model
+file_id = fopen('arm.obj');
+scan_data = textscan(file_id,'%s');
+fclose(file_id);
+scan_data = scan_data{1};
+
+v_ind = find(strcmp(scan_data,'v')==1);
+
+v_posi_X = -str2double(scan_data(v_ind+1));
+v_posi_Y = str2double(scan_data(v_ind+2));
+v_posi_Z = -str2double(scan_data(v_ind+3));
+
+% Truncate the hand model------------------------
+trunc_ind = (v_posi_Y > Hand_Truncate_Threshold);
+v_posi_X = v_posi_X(trunc_ind);
+v_posi_Y = v_posi_Y(trunc_ind);
+v_posi_Z = v_posi_Z(trunc_ind);
+% -----------------------------------------------
+
+% Create a instance of the 'ObjModel' class
+m_obj = ObjModel(v_posi_X, v_posi_Y, v_posi_Z);
+% -------------------------------------------------------------------------
+% Location information identification
+
+plane_v_posi = zeros(3,3); % Vertices position of the plane
+% Center of digit I:
+[~, digitI_bottom_ind] = min(v_posi_Z);
+v_ind1 = getDist(m_obj, digitI_bottom_ind, Digit_Diameter);
+plane_v_posi(1,1) = mean(v_posi_X(v_ind1));
+plane_v_posi(1,2) = mean(v_posi_Y(v_ind1));
+plane_v_posi(1,3) = mean(v_posi_Z(v_ind1));
+
+% Center of digit III:
+[~, digitI_tip_ind] = max(v_posi_Y);
+v_ind2 = getDist(m_obj, digitI_tip_ind, Digit_Diameter);
+plane_v_posi(2,1) = mean(v_posi_X(v_ind2));
+plane_v_posi(2,2) = mean(v_posi_Y(v_ind2));
+plane_v_posi(2,3) = mean(v_posi_Z(v_ind2));
+
+% Center of digit V:
+[~, digitV_top_ind] = max(v_posi_Z);
+v_ind3 = getDist(m_obj, digitV_top_ind, Digit_Diameter);
+plane_v_posi(3,1) = mean(v_posi_X(v_ind3));
+plane_v_posi(3,2) = mean(v_posi_Y(v_ind3));
+plane_v_posi(3,3) = mean(v_posi_Z(v_ind3));
+
+% Plane dividing palmar and dorsal hand (Defined by a*x+b*y+c*z=1)
+hand_plane = plane_v_posi\ones(3,1);
+% palm_ind = ((m_obj.v_posi * hand_plane) > 1);
+% dorsum_ind = ~palm_ind;
+
+% -------------------------------------------------------------------------
+acc_posi = [81.91  223.9  16.06  % 1
+            68.2   227.9  14.05  % 2
+            90.92  208.9  30.02  % 3
+            103.7  203.9  32.93  % 4
+            42.89  212.9  -13.52 % 5
+            108.6  177.9  47.05  % 6
+            28.75  211.9  -16.95 % 7
+            68.88  211.9  22.06  % 8
+            90.89  194.8  37.01  % 9
+            107.9  165.9  47.43  % 10
+            33.98  198.7  -4.984 % 11
+            70.33  170.9  33.05  % 12
+            68.8   180.9  23.93  % 13
+            85.47  164.9  35.05  % 14
+            51.05  185.9  21.05  % 15
+            93.88  155.9  36.14  % 16
+            44.89  177.9  0.59   % 17
+            25.79  176.9  -2.95  % 18 
+            49.89  151.9  33.73  % 19
+            66.08  142.9  36.96  % 20
+            82.89  131.9  39.77  % 21
+            2.07   146.9  -36.95 % 22
+            30.89  152.9  21.7   % 23
+            6.49   124.9  -16.95 % 24  
+            42.12  118.9  38.99  % 25
+            12.09  103.9  6.05   % 26
+            45.89  97.88  45.49  % 27
+            27.89  100.9  37.64  % 28
+            63.36  75.88  48.05  % 29
+            13.64  83.88  23.05  % 30
+            ];
+ 
+acc_num = size(acc_posi,1);
+
+sim_radius = 50;    
+
+% dist_map = zeros(m_obj.v_num,acc_num);
+% tic
+% for sim_i = 1:acc_num
+%     acc_i = findVertex(m_obj, acc_posi(sim_i,:));
+%     waveSim(m_obj, acc_i, -hand_plane, sim_radius, 0);
+%     dist_map(:,sim_i) = m_obj.v_sim;
+%     fprintf('Simulation time = %.2f min\n', (toc/60));
+% end
+      
+%% Plot the hand
+if 1 %---------------------------------------------------------------Switch
+figure('Position',get(0,'ScreenSize').*[0,0,1,0.92])
+% for sim_i = 1:acc_num
+for sim_i = 1
+    v_color = repmat(Skin_Color,[m_obj.v_num,1]);
+%     v_color(:,1) = dist_map(:,sim_i)/sim_radius;
+    scatter3(v_posi_X, v_posi_Y, v_posi_Z,[],v_color,'.')
+    hold on
+    scatter3(acc_posi(:,1), acc_posi(:,2), acc_posi(:,3),...
+        'filled', 'MarkerFaceColor','m', 'MarkerEdgeColor','m')
+    scatter3(acc_posi(sim_i,1), acc_posi(sim_i,2), acc_posi(sim_i,3),...
+        'filled', 'MarkerFaceColor','r')    
+    xlabel('X')
+    ylabel('Y')
+    zlabel('Z')
+    axis equal
+    view(-hand_plane)
+end
+
+[X,Y] = meshgrid(xlim,ylim);
+Z = (-hand_plane(1).*X -hand_plane(2).*Y +1)./hand_plane(3);
+surf(X,Y,Z)
+end %----------------------------------------------------------------Switch
---
+++ WaveReconstruction/ObjModel.m
@@ -0,0 +1,225 @@
+% Class: 3D model loaded from .obj file
+% Constructor inputs:
+%   1. 'posi_X' - a vector containing vertex position X
+%   2. 'posi_Y' - a vector containing vertex position Y
+%   3. 'posi_Z' - a vector containing vertex position Z
+classdef ObjModel < handle
+% Author: Yitian Shao (yitianshao@ucsb.edu)
+% Created on 09/06/2017
+properties %---------------------------------------------------------------
+    v_posi % Vertex position ([posi_X posi_Y posi_Z])
+    v_num % Vertex number
+    v_sim % Vertex value based on simulation (Zeros when initialized)
+end
+properties(GetAccess = private)
+    Step_Size = 2; % [private] Step size of propagation 
+    
+    sim_r % [private] Wave propagation simulation radius (distance limitation)
+end
+methods %------------------------------------------------------------------
+    %% Constructor
+    function obj = ObjModel(posi_X, posi_Y, posi_Z)
+        if nargin == 3
+            if isnumeric(posi_X) && isnumeric(posi_Y) &&...
+                    isnumeric(posi_Z)
+                obj.v_num = length(posi_X);
+                if (length(posi_Y) == obj.v_num) &&...
+                        (length(posi_Z) == obj.v_num)
+                    disp('Model successfully loaded');
+                else
+                    error('Failed to load the model: all input vectors (X, Y, Z) must have the same length');
+                end
+                
+                if size(posi_X,2) ~= 1
+                    posi_X = posi_X';
+                end
+                if size(posi_Y,2) ~= 1
+                    posi_Y = posi_Y';
+                end
+                if size(posi_Z,2) ~= 1
+                    posi_Z = posi_Z';
+                end
+                obj.v_posi = [posi_X, posi_Y, posi_Z];
+                
+                obj.v_sim = []; 
+            else
+                error('Value must be numeric')
+            end
+        end
+    end
+    
+    %% Function: Get distance between two vertices
+    function [v_ind, v_dist] = getDist(obj, center_i, search_r)
+    % Get distance from vertices within certain area to its center vertex
+    %----------------------------------------------------------------------
+    % [v_ind, v_dist] = getDist(obj, center_i, search_r)
+    %
+    % Inputs:
+    % 1. 'obj' - an instance of the 'ObjModel' class
+    % 2. 'center_i' - index of the vertex located at the center of search area
+    % 3. 'search_r' - radius of search area
+    %
+    % Outputs:
+    % 1. 'v_ind' - index of vertices within search area
+    % 2. 'v_dist' - distance between vertices within search area and the center
+    %----------------------------------------------------------------------
+    ind = (obj.v_posi(:,1) < (obj.v_posi(center_i,1) + search_r));
+    ind = ind & (obj.v_posi(:,1) > (obj.v_posi(center_i,1) - search_r));
+    ind = ind & (obj.v_posi(:,2) < (obj.v_posi(center_i,2) + search_r));
+    ind = ind & (obj.v_posi(:,2) > (obj.v_posi(center_i,2) - search_r));
+    ind = ind & (obj.v_posi(:,3) < (obj.v_posi(center_i,3) + search_r));
+    ind = ind & (obj.v_posi(:,3) > (obj.v_posi(center_i,3) - search_r));
+    v_dist = ((obj.v_posi(ind,1)-obj.v_posi(center_i,1)).^2 +...
+              (obj.v_posi(ind,2)-obj.v_posi(center_i,2)).^2 +...
+              (obj.v_posi(ind,3)-obj.v_posi(center_i,3)).^2).^0.5;
+    v_ind = find(ind == 1);
+    
+    % Exclude vertices outside the searching sphere and the center vertex
+    ind_in = (v_dist < search_r) & (v_ind ~= center_i);
+    v_ind = v_ind(ind_in);
+    v_dist = v_dist(ind_in);
+    end
+    
+    %% Function: Propagation vector
+    function [v_ind, p_vect, v_dist] = getPropVect(obj, center_i, search_r)
+    %----------------------------------------------------------------------
+    ind = (obj.v_posi(:,1) < (obj.v_posi(center_i,1) + search_r));
+    ind = ind & (obj.v_posi(:,1) > (obj.v_posi(center_i,1) - search_r));
+    ind = ind & (obj.v_posi(:,2) < (obj.v_posi(center_i,2) + search_r));
+    ind = ind & (obj.v_posi(:,2) > (obj.v_posi(center_i,2) - search_r));
+    ind = ind & (obj.v_posi(:,3) < (obj.v_posi(center_i,3) + search_r));
+    ind = ind & (obj.v_posi(:,3) > (obj.v_posi(center_i,3) - search_r));
+    v_ind = find(ind == 1);
+        
+    p_vect = bsxfun(@minus,obj.v_posi(v_ind,:),obj.v_posi(center_i,:));
+    
+
+    % Exclude the center vertex
+    ind_in = (v_ind ~= center_i);
+    v_ind = v_ind(ind_in);
+    p_vect = p_vect(ind_in,:);
+    
+    v_dist = (p_vect(:,1).^2 +p_vect(:,2).^2 +p_vect(:,3).^2).^0.5;
+    end
+    
+    %% Function: Find vertex by their position
+    function v_ind = findVertex(obj, search_posi)
+        search_r = 0.01;
+        find_v_num = 0;
+        while (~find_v_num)
+            ind = (obj.v_posi(:,1) < (search_posi(1) + search_r));
+            ind = ind & (obj.v_posi(:,1) > (search_posi(1) - search_r));
+            ind = ind & (obj.v_posi(:,2) < (search_posi(2) + search_r));
+            ind = ind & (obj.v_posi(:,2) > (search_posi(2) - search_r));
+            ind = ind & (obj.v_posi(:,3) < (search_posi(3) + search_r));
+            ind = ind & (obj.v_posi(:,3) > (search_posi(3) - search_r));
+            v_ind = find(ind > 0);
+            find_v_num = length(v_ind);
+            if (~find_v_num)
+                search_r = search_r +0.001;
+            elseif (find_v_num > 1)
+                search_r = search_r -0.001;
+            end
+        end
+    end
+    
+    %% Function: Wave propagation simulation
+    function fig_h = waveSim(obj, acc_i, view_direct, sim_limit, is_disp)
+    % Compute propagation distance from selected accelerometer to each vertex
+    %----------------------------------------------------------------------
+    % waveSim(obj, acc_i, view_direct, sim_limit)
+    %
+    % Inputs:
+    % 1. 'obj' - an instance of the 'ObjModel' class
+    % 2. 'acc_i' - index of the vextex where the selected accelerometer locates
+    % 3. 'view_direct' - a vector indicating the direction of the view
+    % 4. 'sim_limit' - wave propagation distance limitation for the simulation
+    % Optional inputs:
+    % 5. 'is_disp' - Display the simulation process if set to be 1
+    %
+    % Outputs:
+    % 1. 'fig_h' = handle of the figure displaying the simulation process
+    %----------------------------------------------------------------------
+    Model_Color = [255,223,196]/255; % Default (color of skin)
+    % ---------------------------------------------------------------------
+    if nargin < 5
+        is_disp = 1; % Display the simulation process by default
+    end
+        
+    % Display propagation simulation
+    if is_disp
+        fig_h = figure('Position',get(0,'ScreenSize').*[0,0,1,1]);
+        scatter3(obj.v_posi(:,1), obj.v_posi(:,2), obj.v_posi(:,3),...
+            '.', 'MarkerEdgeColor',Model_Color);
+        hold on
+        scatter3(obj.v_posi(acc_i,1), obj.v_posi(acc_i,2), obj.v_posi(acc_i,3),...
+            'filled', 'MarkerFaceColor','r');
+        xlabel('X');
+        ylabel('Y');
+        zlabel('Z');
+        axis equal
+        view(view_direct);
+    else
+        fig_h = [];
+    end
+    
+    obj.sim_r = sim_limit; % Simulation distance limitation
+    
+    % Initialize all vertices with largest distance value
+    obj.v_sim = ones(obj.v_num,1); 
+    obj.v_sim = obj.v_sim .*sim_limit;    
+    obj.v_sim(acc_i) = 0; % Distance value for accelerometer vertex is zero
+    
+    propagate(obj, acc_i, [0,0,0], is_disp);
+
+    end
+
+    %% Recursive function: Propagate 
+    function propagate(obj, center_i, curr_direct, is_disp)
+    % Recursive function for propagation simulation 
+    %----------------------------------------------------------------------
+    % [] = propagate(m_obj, center_i, search_r)
+    %
+    % Inputs:
+    % 1. 'obj' - an instance of the 'ObjModel' class
+    % 2. 'center_i' - index of the vertex located at the center of search area
+    % 3. 'curr_direct' - current propagation direction
+    % 4. 'is_disp' - Display the simulation process if set to be 1
+    %
+    % Outputs:
+    % 1. '' - 
+    %----------------------------------------------------------------------
+    if (obj.v_sim(center_i) < obj.sim_r) % Current distance within searching range
+        [v_ind, p_vect, v_dist]=getPropVect(obj, center_i, obj.Step_Size); 
+        
+        adj_v_num = length(v_ind); % Number of adjacent vertices
+        v_dist = v_dist +obj.v_sim(center_i); % Total distance  
+        
+        f_direct = 0.5*(curr_direct*curr_direct');
+        
+        if (adj_v_num > 0) % Exist adjacent vertices
+            s_seq = randperm(adj_v_num); % Randomize searching sequence
+            for i = s_seq
+                if v_dist(i) < obj.v_sim(v_ind(i)) % Discovered a shorter path
+                    obj.v_sim(v_ind(i)) = v_dist(i);
+
+                    % Update the change on display (Slow down significantly!)
+                    if is_disp
+                        scatter3(obj.v_posi(v_ind(i),1),...
+                            obj.v_posi(v_ind(i),2),obj.v_posi(v_ind(i),3),...
+                            '.', 'MarkerEdgeColor',...
+                            ones(1,3)*v_dist(i)/obj.sim_r);
+                        drawnow;   
+                    end
+                    
+                    p_direct = curr_direct*p_vect(i,:)'; % Propagation direction 
+                    if (p_direct >= f_direct) % Forward propagation only
+                        propagate(obj, v_ind(i), p_vect(i,:), is_disp); % Propagate to an adjacent vertex
+                    end
+                end
+            end
+        end
+    end
+    end
+end
+end
