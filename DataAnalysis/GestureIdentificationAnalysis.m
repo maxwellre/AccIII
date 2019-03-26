da82e48297fe3af141aea991ef064bc1d66a0df5
maxwellre <yitianshao@gmail.com>
2019-03-26 13:31:57 -0700

    Preprocess all AccIII measurements. Downsampled to 1294 Hz. Detrended and projected to direction of the principle component of the data.

--- DataAnalysis/GestureIdentificationAnalysis.m
+++ DataAnalysis/GestureIdentificationAnalysis.m
@@ -1,6 +1,8 @@
 %% Gesture Identification Analysis
 % Created on 03/25/2019
 %--------------------------------------------------------------------------
+ChannelNum = 46;
+
 Data_Path = 'Data';
 Measure_Freq = [20,50,100,200,250,300,500];
 Measure_Type = sprintfc('%03dHz_sine',Measure_Freq);
@@ -10,7 +12,7 @@ temp = {'ClickMouse_II_III','GrabHandleAndRelease','GrabTengenri',...
 Measure_Type = [Measure_Type,temp];
 data_num = length(Measure_Type);
 
-acc_ind = setdiff(1:46,[10,20,30,40]);
+acc_ind = setdiff(1:ChannelNum,[10,20,30,40]);
 disp_num = length(acc_ind);
 
 if ~( exist('acc_data','var')&&exist('t','var')&&exist('Fs','var') )
@@ -32,6 +34,31 @@ if ~( exist('acc_data','var')&&exist('t','var')&&exist('Fs','var') )
 end
 
 %--------------------------------------------------------------------------
+% Data Preprocessing
+dsFreq = 1294; % Downsample to 1294 Hz
+segDuration = 5; % 5 sec duration
+tSync = 0:(1/dsFreq):(5 -1/dsFreq); % Synchronized time stamps
 
-
+accAmp = cell(data_num,1);
+for i = 1:data_num
+    accAmp{i} = zeros(segDuration*dsFreq,ChannelNum);
+    
+%    % RMS amplitude (Inaccurate representation)
+%     for ax_i = 1:3
+%         temp = acc_data{i}(:,:,ax_i);
+%         temp = resample(temp,dsFreq*10,round(Fs(i)*10));       
+%         temp = detrend(temp,'constant');
+%         accAmp{i} = accAmp{i} + temp(1:(segDuration*dsFreq),:).^2;        
+%     end
+%     accAmp{i} = accAmp{i}.^0.5;
+    
+    % Projected waveform
+    for ch_i = acc_ind
+        temp = squeeze(acc_data{i}(:,ch_i,:));
+        temp = resample(temp,dsFreq*10,round(Fs(i)*10));       
+        temp = detrend(temp(1:(segDuration*dsFreq),:),'constant');
+        [~,score,~] = pca(temp); % PCA
+        accAmp{i}(:,ch_i) = score(:,1);
+    end
+end
 
