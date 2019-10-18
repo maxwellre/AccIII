69f170a1e0ad4c0cd71537ec83e82fe35bd64d95
maxwellre <yitianshao@gmail.com>
2019-10-18 16:58:45 -0700

    Simplified AccIII

---
+++ AccIIIQtUI/AccIIIQtUI/include/DataProc.h
@@ -0,0 +1,12 @@
+#pragma once
+//------------------------------------------------------------------------------
+#include<string>
+#define ExpFs 1310 // Expected sampling frequency
+#define AccBusNum 23 // Totally 23 buses each connecting two accelerometers
+#define DataByteNum 24 //Each bus connects two accelerometer each with 3 axies [High + Low] bytes
+
+void USBReadData(FT_HANDLE ftHandle, DWORD readBytes, long* dwSum, int dataNum, unsigned char fileBuffer[]);
+
+void SaveDataResult(long dwSum, unsigned char fileBuffer[]);
+
+void SaveNum(float inputValue, std::string fileName);
