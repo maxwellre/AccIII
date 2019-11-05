ab276f34ae30b185089f7baf619d27212c3bb469
maxwellre <yitianshao@gmail.com>
2019-11-04 18:44:59 -0800

    Data analysis for TwinAcc measurement

--- AccIIITwin/MATLAB_Analysis/DataInspection.m
+++ AccIIITwin/MATLAB_Analysis/DataInspection.m
@@ -1,6 +1,102 @@
-dataPath = '.\Tap Plamar\TapPalm_Loc1.mat';
+% -------------------------------------------------------------------------
+addpath('../../WaveReconstructModel/');
+% -------------------------------------------------------------------------
+
+dataPath = '.\Tap Plamar\TapPalm_Loc9.mat';
 
 TrialNum = 11;
-correct_factor = [0.9993 1.0090]; % Mean factor (STD = 0.0008 Hz)
+correctFactor = [0.9993 1.0090]; % Mean factor (STD = 0.0008 Hz)
 
-dataSeg = segmentData(dataPath, TrialNum, correct_factor);
+arrayLabel = {'Palm', 'Dorsum'};
+% -------------------------------------------------------------------------
+if ~exist('dist_map','var')
+    load('SimRadius64mm_StepSize2mm_120Degree_42Acc.mat','dist_map');
+end
+if ~exist('m_obj','var')
+    load('SimRadius64mm_StepSize2mm_120Degree_42Acc.mat','m_obj');
+end
+% -------------------------------------------------------------------------
+% Data segmentation
+dataSeg = segmentData(dataPath, TrialNum, 0, correctFactor);
+
+if 0 % -------------------------------------------------------------------- 
+%% (Optional) show all signals
+dispInd = 1:100;
+figure('Position',get(0,'ScreenSize').*[20,20,0.9,0.9]);
+for i = 1:TrialNum
+    for j = 1:3
+        subplot(TrialNum,6,(i-1)*6+j)
+        plot(dataSeg{i,1}(dispInd),dataSeg{i,2}(dispInd,:,j))
+    end
+    for j = 1:3
+        subplot(TrialNum,6,(i-1)*6+3+j)
+        plot(dataSeg{i,3}(dispInd),dataSeg{i,4}(dispInd,:,j))
+    end
+end
+end
+
+%% Process data for visualization
+accInd = setdiff(1:46,[10,20,30,40]);
+accNum = length(accInd);
+
+%--------------------------------------------------------------------------
+avgLen = 0.1; % Average time length (secs) 
+avgSegRMSEn = nan(2,TrialNum,accNum); % Averaged segment RMS energy
+for i = 1:TrialNum % Trial index
+    for b = 1:2 % Board index
+        t = dataSeg{i,2*b-1};
+        t = t - t(1);
+        avgEndInd = find(t >= avgLen, 1);
+        
+        accData = dataSeg{i,2*b};
+        
+        % DC-filtering -----------------------------------
+        accData = bsxfun(@minus, accData, mean(accData,1));
+        
+        for j = 1:accNum % Accelerometer index
+            temp = squeeze(accData(1:avgEndInd,accInd(j),:));
+            
+            amp = (temp(:,1).^2 + temp(:,2).^2 + temp(:,3).^2).^0.5;
+            avgSegRMSEn(b,i,j) = rms(amp);
+        end       
+    end
+end
+
+%--------------------------------------------------------------------------
+%% 3D plot
+% Interpolation weight construction
+Phi = 17./(dist_map+25.5) - 0.087;
+Phi(Phi < 0) = 0;
+Phi = bsxfun(@rdivide, Phi, sum(Phi,2));
+Phi(isnan(Phi)) = 0; 
+
+v_color{1} = Phi*(squeeze(avgSegRMSEn(1,:,:))'); % A
+v_color{2} = Phi*(squeeze(avgSegRMSEn(2,:,:))'); % B
+
+cmap = jet(1000);
+%--------------------------------------------------------------------------
+Default_View = [-100 75];
+ctext = 'k'; % Color of axes and text
+
+fig1 = figure('Position',get(0,'ScreenSize').*[20,80,0.98,0.8]);
+set(fig1, 'Color', 'w');
+
+for b = 1:2
+    color_range = [min(v_color{b}(:)), max(v_color{b}(:))];   
+    for j = 1:10 % Show only last ten trials
+        subplot(4,5,(b-1)*10+j)
+        colormap(cmap);
+        scatter3(m_obj.v_posi(:,1), m_obj.v_posi(:,2), m_obj.v_posi(:,3),...
+            2,v_color{b}(:,j+1),'Filled')
+        axis equal
+        view(Default_View)
+ 
+        currLabel = sprintf('%s-%d',arrayLabel{b},j);
+        title(currLabel);
+
+        fprintf('%s [Range: %.2f - %.2f]\n',currLabel, caxis);
+        axis off
+        set(gca,'FontSize',8,'Color','none','XColor',ctext,'YColor',ctext,...
+            'ZColor',ctext)
+    end
+end
--- AccIIITwin/MATLAB_Analysis/segmentData.m
+++ AccIIITwin/MATLAB_Analysis/segmentData.m
@@ -1,47 +1,77 @@
-function output_data = segmentData(data_path, trialNum, correct_factor)
-load(data_path);
+%% Test and display the accelerometer array data (Design III)
+% Using LDV to caliberate the accelerometer array
+% Inputs:
+%   1. 'dataPath' - Path of the .bin file that stores the data
+%   2. 'trialNum' - Number of repeated measurement for each configuration
+% Optional inputs:
+%   3. 'inspectSeg' - display onset detection for inspection
+%   4. 'correctFactor' - samping frequency correction factor
+% Outputs:
+%   1. 'output_data' - Segmentated data [t_A | data_A | t_B | data_B]
+%--------------------------------------------------------------------------
+function output_data = segmentData(dataPath, trialNum, inspectSeg,...
+    correctFactor)
+% Created on 11/04/2019 
+% Created by Yitian Shao (yitianshao@ucsb.edu)
+%--------------------------------------------------------------------------
+if nargin < 3
+    inspectSeg = 0;
+end
+if nargin < 4
+    correctFactor = [1 1];
+end
+
+load(dataPath);
 
 % Sampling frequency and time stamps correction (for old data)
-Fs_A = Fs_A*correct_factor(1);
-Fs_B = Fs_B*correct_factor(2);
-t_A = t_A./correct_factor(1);
-t_B = t_B./correct_factor(2);
+Fs_A = Fs_A*correctFactor(1);
+Fs_B = Fs_B*correctFactor(2);
+t_A = t_A./correctFactor(1);
+t_B = t_B./correctFactor(2);
 
+% Compute total energy of all channels and axes
 accEnA = acc_data_A(:,:,1).^2 +acc_data_A(:,:,2).^2 +acc_data_A(:,:,3).^2;
 accEnB = acc_data_B(:,:,1).^2 +acc_data_B(:,:,2).^2 +acc_data_B(:,:,3).^2; 
-
 accTotalEnA = sum(accEnA,2);
 accTotalEnB = sum(accEnB,2);
 
+% Onset detection using total energy (A)
 temp = accTotalEnA - mean(accTotalEnA);
-temp = movmean(temp,40); % Smooth
+temp = movmean(temp,100); % Smooth
 thresh = 0.5*max(temp);
 onset_ind = (temp > thresh);
 onset_d = diff(onset_ind);
 onset_A = find(onset_d == 1);
 
+% Check onset detection result (A)
 if (length(onset_A) ~= trialNum)
     error('Onset detection for array A is incorrect!')
 end
 
+% Onset detection using total energy (B)
 temp = accTotalEnB - mean(accTotalEnB);
-temp = movmean(temp,40); % Smooth
+temp = movmean(temp,100); % Smooth
 thresh = 0.5*max(temp);
 onset_ind = (temp > thresh);
 onset_d = diff(onset_ind);
 onset_B = find(onset_d == 1);
 
+% Check onset detection result (B)
 if (length(onset_B) ~= trialNum)
     error('Onset detection for array B is incorrect!')
 end
 
+% (Optional) display onset detection results
+if inspectSeg
 figure; 
 yyaxis left; plot(t_A,accTotalEnA); ylabel('Palm (A) Total Energy');
 hold on; plot(t_A(onset_A),mean(accTotalEnA),'+r');
 yyaxis right; plot(t_B,accTotalEnB); ylabel('Dorsum (B) Total Energy');
 hold on; plot(t_B(onset_B),mean(accTotalEnB),'+k');
 xlabel('Time (secs)')
+end
 
+% Data segmentation using detection onset ticks
 output_data = cell(trialNum,4);
 for t_i = 1:(trialNum-1)
     ind_A = onset_A(t_i):(onset_A(t_i+1)-1);
@@ -56,4 +86,5 @@ output_data{trialNum,1} = t_A(onset_A(end):end);
 output_data{trialNum,2} = acc_data_A(onset_A(end):end,:,:);
 output_data{trialNum,3} = t_B(onset_B(end):end);
 output_data{trialNum,4} = acc_data_B(onset_B(end):end,:,:);
+disp('Segmentated data format: [t_A | data_A | t_B | data_B]')
 end
